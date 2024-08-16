d14ccd8dabd6318061aba891b6d59b00496c70d5
monsters <2511289336@qq.com>
2024-08-16 23:43:39 +0800

    feat: pipe

---
+++ pipe.go
@@ -0,0 +1,46 @@
+package m
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"reflect"
+)
+
+const (
+	CtxQuery = "query"
+	CtxBody  = "body"
+)
+
+func QueryPipe[T any]() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var instance T
+
+		reflectInstance := reflect.ValueOf(&instance).Elem()
+
+		if err := c.ShouldBindQuery(reflectInstance.Addr().Interface()); err != nil {
+			c.JSON(http.StatusForbidden, gin.H{"code": 1000, "msg": err.Error()})
+			c.Abort()
+			return
+		}
+
+		c.Set(CtxQuery, instance)
+		c.Next()
+	}
+}
+
+func BodyPipe[T any]() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var instance T
+
+		reflectInstance := reflect.ValueOf(&instance).Elem()
+
+		if err := c.ShouldBindBodyWithJSON(reflectInstance.Addr().Interface()); err != nil {
+			c.JSON(http.StatusForbidden, gin.H{"code": 1000, "msg": err.Error()})
+			c.Abort()
+			return
+		}
+
+		c.Set(CtxBody, instance)
+		c.Next()
+	}
+}
